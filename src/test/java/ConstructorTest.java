daa72c48229a07a391ea050b2ca74e3befd50443
Yuri Tsvetkov <ys.tsvetkov@gmail.com>
2023-04-09 23:37:59 +0300

    Revert "Исправил проверки нажатий на табы в "Соберите бургер", Загрузил новые результаты теста"
    This reverts commit 985cb0800135a69837367a40ff99dcbd7f19e310.

--- src/test/java/ConstructorTest.java
+++ src/test/java/ConstructorTest.java
@@ -1,7 +1,6 @@
 import io.qameta.allure.junit4.DisplayName;
 import static org.junit.Assert.assertEquals;
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
@@ -10,7 +9,6 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import pages.HomePage;
 
 public class ConstructorTest {
-
     private WebDriver driver;
     private String expectedTextConstructorTitle = "Соберите бургер";
     private String expectedTextBuns = "Булки";
@@ -47,27 +45,31 @@ public class ConstructorTest {
         String actualTextConstructorTitle = homePage.getTextConstructorTitle();
         assertEquals("Не корректный результат", expectedTextConstructorTitle, actualTextConstructorTitle);
     }
+
     @Test
-    @DisplayName("Перелючение в раздел Булки")
-    public void successEnterToBunsTest() {
+    @DisplayName("Тест перехода в раздел Соусы в конструкторе")
+    public void successEnterToSaucesTest() {
         HomePage homePage = new HomePage(driver);
-        Assert.assertEquals("tab_tab__1SPyG tab_tab_type_current__2BEPc pt-4 pr-10 pb-4 pl-10 noselect", homePage.getBunClass());
+        homePage.clickSaucesButton();
+        String actualResult = driver.findElement(homePage.getCurrentSection()).getText();
+        assertEquals("Не корректный результат", expectedTextSauces, actualResult);
     }
     @Test
-    @DisplayName("Переключение в раздел Соусы")
-    public void successEnterToSauceTest() {
-        HomePage homePage = new HomePage(driver);
-        homePage.btnSauceClick();
-        Assert.assertEquals("tab_tab__1SPyG tab_tab_type_current__2BEPc pt-4 pr-10 pb-4 pl-10 noselect", homePage.getSauceClass());
-    }
-    @Test
-    @DisplayName("Переключение в раздел Ингредиенты")
+    @DisplayName("Тест перехода в раздел Начинки в конструкторе")
     public void successEnterToFillingsTest() {
         HomePage homePage = new HomePage(driver);
-        homePage.btnFillingsClick();
-        Assert.assertEquals("tab_tab__1SPyG tab_tab_type_current__2BEPc pt-4 pr-10 pb-4 pl-10 noselect", homePage.getFillingsClass());
+        homePage.clickFillingsButton();
+        String actualResult = driver.findElement(homePage.getCurrentSection()).getText();
+        assertEquals("Не корректный результат", expectedTextFillings, actualResult);
     }
-
+    @Test
+    @DisplayName("Тест перехода в раздел Булки в конструкторе")
+    public void successEnterToBunsTest() {
+        HomePage homePage = new HomePage(driver);
+        homePage.clickBunsButton();
+        String actualResult = driver.findElement(homePage.getCurrentSection()).getText();
+        assertEquals("Не корректный результат", expectedTextBuns, actualResult);
+    }
     @After
     public void teardown() {
         driver.quit();
